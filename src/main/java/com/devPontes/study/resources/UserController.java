3cc977d5f38cd9f0b8a4282c890d42912583603f
Pedro Pontes <pedrohenriquepsantos2000@gmail.com>
2024-01-03 19:53:16 -0300

    Service layer, component registration

--- src/main/java/com/devPontes/study/resources/UserController.java
+++ src/main/java/com/devPontes/study/resources/UserController.java
@@ -1,20 +1,35 @@
 package com.devPontes.study.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devPontes.study.entities.User;
+import com.devPontes.study.services.UserServices;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+	@Autowired
+	private UserServices service;
+	
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Pedro", "pedro@gmail.com", "99999999", "12345");
-	    return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
