d37c86fd5a456f1f8003bd61782a664994994280
Pedro Pontes <pedrohenriquepsantos2000@gmail.com>
2024-01-04 17:35:46 -0300

    Entidade Product

--- src/main/java/com/devPontes/study/config/TestConfig.java
+++ src/main/java/com/devPontes/study/config/TestConfig.java
@@ -10,9 +10,11 @@ import org.springframework.context.annotation.Profile;
 
 import com.devPontes.study.entities.Category;
 import com.devPontes.study.entities.Order;
+import com.devPontes.study.entities.Product;
 import com.devPontes.study.entities.User;
 import com.devPontes.study.repositories.CategoryRepository;
 import com.devPontes.study.repositories.OrderRepository;
+import com.devPontes.study.repositories.ProductRepository;
 import com.devPontes.study.repositories.UserRepository;
 
 @Configuration
@@ -28,14 +30,25 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private ProductRepository productRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-	
-		
+
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "",
+				null);
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "", null);
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "", null);
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "", null);
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "",
+				null);
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 		User u1 = new User(null, "Pedro Lopes", "lopes@gmail.com", "977777777", "123456");
 		User u2 = new User(null, "Maria Rita", "maria@gmail.com", "988888888", "123456");
 		userRepository.saveAll(Arrays.asList(u1, u2));
--- src/main/java/com/devPontes/study/entities/Category.java
+++ src/main/java/com/devPontes/study/entities/Category.java
@@ -1,13 +1,16 @@
 package com.devPontes.study.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -19,6 +22,9 @@ public class Category implements Serializable{
 	private Long id;
 	private String name;
 
+	@Transient
+	private Set<Product> products = new HashSet<>();
+	
 	public Category(Long id, String name) {
 		this.id = id;
 		this.name = name;
@@ -43,6 +49,12 @@ public class Category implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -65,6 +77,5 @@ public class Category implements Serializable{
 	public String toString() {
 		return "Category [id=" + id + ", name=" + name + "]";
 	}
-	
 	
 }
