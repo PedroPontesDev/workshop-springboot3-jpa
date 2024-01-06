f6c5ccc38ebc8db49b0e02f3b5963b24dc9080bf
Pedro Pontes <pedrohenriquepsantos2000@gmail.com>
2024-01-06 11:38:43 -0300

    Associação Many-To-Many com JoinTable

--- src/main/java/com/devPontes/study/entities/Product.java
+++ src/main/java/com/devPontes/study/entities/Product.java
@@ -9,24 +9,27 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
 
-	@Transient
+	@ManyToMany
+	@JoinTable(name = "product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
 	public Product(Long id, String name, String description, Double price, String imgUrl, Set<Category> categories) {
@@ -83,6 +86,9 @@ public class Product implements Serializable{
 	}
 
 	public Set<Category> getCategories() {
+		if (categories == null) {
+			categories = new HashSet<>();
+		}
 		return categories;
 	}
 
@@ -108,8 +114,5 @@ public class Product implements Serializable{
 		return "Product [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
 				+ ", imgUrl=" + imgUrl + ", categories=" + categories + "]";
 	}
-	
-	
-	
-	
+
 }
